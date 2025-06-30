121c87572c9784476f4605b34f49a48bfaf29709
Hritik113 <hritikkumarraut03@gmail.com>
2025-06-30 19:28:05 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-// vite.config.ts
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/hritik-kumar-portfolio/', // 👈 this is crucial
-  // other config...
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
 });
-
